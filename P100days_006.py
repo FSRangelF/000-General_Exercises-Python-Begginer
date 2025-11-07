8e21bb6545c61a42d4eac1381bbfd9f72d471091
FSRangelF <fs.rangel@gmail.com>
2025-11-07 15:36:40 -0300

    revision

--- P100days_006.py
+++ P100days_006.py
@@ -5,61 +5,64 @@ def random_color():
     r = random.randint(0,255)
     b = random.randint(0,255)
     g = random.randint(0,255)
-    rgb = (r, g, b)
-    return rgb
+    return (r, g, b)
+
+def spirograph(gap):
+    steps = int(360/gap+1)
+    angle = 0
+    for i in range (steps):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(angle)
+        angle += gap
 
 t.colormode(255)
 
-timmy = t.Turtle()
-timmy.shape("turtle")
-timmy.color("red")
+tim = t.Turtle()
+tim.shape("turtle")
+tim.color("red")
 
 screen = t.Screen()
 
 
-#challenge 1
+# #challenge 1
 for i in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+    tim.forward(100)
+    tim.right(90)
 
-# #challenge 2
+# # #challenge 2
 for i in range (15):
-    timmy.pendown()
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
-# #challenge 3
+# # #challenge 3
 sides = 3
-timmy.pendown()
 while sides <= 10:
     angle = 360/sides
-    timmy.color(random_color())
+    tim.color(random_color())
     for i in range (sides):
-        timmy.forward(100)
-        timmy.rt(angle)
+        tim.forward(100)
+        tim.rt(angle)
     sides += 1
 
 
-# #challenge 4
-timmy.speed("fastest")
-timmy.pensize(10)
+# # #challenge 4
+tim.speed("fastest")
+tim.pensize(10)
 count = 0
-while count < 1000:
+while count < 200:
     angle = 90*random.randint(0,3)
-    timmy.color(random_color())
-    timmy.right(angle)
-    timmy.forward(30)
+    tim.color(random_color())
+    tim.setheading(angle)
+    tim.forward(30)
     count += 1
 
 # #challenge 5
-timmy.speed("fastest")
-timmy.pensize(1)
-steps = 36
-angle = 360 / steps
-for i in range (steps):
-    timmy.color(random_color())
-    timmy.circle(100)
-    timmy.rt(angle)
+tim.speed("fastest")
+tim.pensize(1)
+spirograph(3.6)
+
         
 screen.exitonclick()
